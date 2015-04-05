3d624761137ac7efd071b1a77956b74df0b890ae
Abas Setiawan <sukasenyumm@gmail.com>
2015-04-05 21:27:55 +0700

    - add CheckNeuronMap method

--- Tugas/Tugas/Map.cs
+++ Tugas/Tugas/Map.cs
@@ -9,56 +9,31 @@ namespace Tugas
         private string result;
         private string learningtime;
         private Neuron[,] outputs;  // Collection of weights.
-
         private int iteration;      // Current iteration.
-
         private int length;        // Side length of output grid.
-
         private int dimensions;    // Number of input dimensions.
-
         private Random rnd = new Random();
 
-
-
         private List<string> labels = new List<string>();
-
         private List<double[]> patterns = new List<double[]>();
-
-
-
-        //static void Main(string[] args)
-        //{
-
-        //    new Map(25, 2, "hwDataTraining.csv");
-
-        //    Console.ReadLine();
-
-        //}
-
-
 
         public Map(int dimensions, int length, string file)
         {
 
             DateTime startTime = DateTime.Now;
-
             this.length = length;
-
             this.dimensions = dimensions;
-
             Initialise();
-
             LoadData(file);
-
-            NormalisePatterns();
-
+            //NormalisePatterns();
             Train(0.0000001);
-
             DumpCoordinates();
 
             TimeSpan runTime = DateTime.Now - startTime;
             Console.WriteLine("Time elapsed: " + Convert.ToString(runTime.TotalMilliseconds / 1000) + " s");
             learningtime = "Time elapsed: " + Convert.ToString(Math.Floor(runTime.TotalMilliseconds / 1000)) + " s";
+
+            CheckNeuronMap();
         }
 
         public string getResult()
@@ -71,7 +46,7 @@ namespace Tugas
         }
         private void Initialise()
         {
-
+            Console.WriteLine("Neuron Initialized.... ");
             outputs = new Neuron[length, length];
 
             for (int i = 0; i < length; i++)
@@ -79,8 +54,8 @@ namespace Tugas
 
                 for (int j = 0; j < length; j++)
                 {
-
                     outputs[i, j] = new Neuron(i, j, length);
+                    Console.WriteLine("Output Neuron {0}: [{1},{2}] weight:", i, outputs[i, j].X, outputs[i, j].Y);
 
                     outputs[i, j].Weights = new double[dimensions];
 
@@ -88,82 +63,61 @@ namespace Tugas
                     {
 
                         outputs[i, j].Weights[k] = rnd.NextDouble();
-
+                        Console.Write("{0:N2} ", outputs[i, j].Weights[k]);
                     }
-
                 }
-
             }
-
         }
-
-
 
         private void LoadData(string file)
         {
 
             StreamReader reader = File.OpenText(file);
-
             reader.ReadLine(); // Ignore first line.
-
             while (!reader.EndOfStream)
             {
 
                 string[] line = reader.ReadLine().Split(',');
-
                 labels.Add(line[0]);
-
                 double[] inputs = new double[dimensions];
-
                 for (int i = 0; i < dimensions; i++)
                 {
-                    //harcoded here
-                    if (double.Parse(line[i + 1]) == 10.0)
-                        inputs[i] = double.Parse(line[i + 1]) - 9.0;
-                    else
+                    ////harcoded here
+                    //if (double.Parse(line[i + 1]) == 10.0)
+                    //    inputs[i] = double.Parse(line[i + 1]) - 9.0;
+                    //else
                         inputs[i] = double.Parse(line[i + 1]);
                 }
-
                 patterns.Add(inputs);
-
             }
-
             reader.Close();
 
         }
 
+        //commented because we don't need to normalize data. (0,1)
+        //private void NormalisePatterns()
+        //{
 
+        //    for (int j = 0; j < dimensions; j++)
+        //    {
 
-        private void NormalisePatterns()
-        {
+        //        double sum = 0;
+        //        for (int i = 0; i < patterns.Count; i++)
+        //        {
+        //            sum += patterns[i][j];
+        //        }
 
-            for (int j = 0; j < dimensions; j++)
-            {
+        //        double average = sum / patterns.Count;
 
-                double sum = 0;
-
-                for (int i = 0; i < patterns.Count; i++)
-                {
-
-                    sum += patterns[i][j];
-
-                }
-
-                double average = sum / patterns.Count;
-
-                for (int i = 0; i < patterns.Count; i++)
-                {
-
-                    patterns[i][j] = patterns[i][j] / average;
-                    //hardcoded here
-                    if (double.IsNaN(patterns[i][j]))
-                        patterns[i][j] = 0;
-
-                }
-
-            }
-
-        }
+        //        for (int i = 0; i < patterns.Count; i++)
+        //        {
+        //            patterns[i][j] = patterns[i][j] / average;
+        //            //hardcoded here
+        //            if (double.IsNaN(patterns[i][j]))
+        //                patterns[i][j] = 0;
+        //        }
+        //    }
+        //}
 
 
 
@@ -171,34 +125,24 @@ namespace Tugas
         {
 
             double currentError = double.MaxValue;
-
+            Console.WriteLine("learning progress... (iteration)");
             while (currentError > maxError)
             {
-
                 currentError = 0;
 
                 List<double[]> TrainingSet = new List<double[]>();
 
                 foreach (double[] pattern in patterns)
                 {
-
                     TrainingSet.Add(pattern);
-
                 }
 
                 for (int i = 0; i < patterns.Count; i++)
                 {
-
                     double[] pattern = TrainingSet[rnd.Next(patterns.Count - i)];
-
                     currentError += TrainPattern(pattern);
-
                     TrainingSet.Remove(pattern);
-
                 }
-
-                Console.WriteLine(currentError.ToString("0.0000000"));
-
             }
 
         }
@@ -207,30 +151,22 @@ namespace Tugas
 
         private double TrainPattern(double[] pattern)
         {
-
             double error = 0;
 
             Neuron winner = Winner(pattern);
+            //Console.WriteLine("Neuron Winner {0},{1}", winner.X, winner.Y);
 
             for (int i = 0; i < length; i++)
             {
-
                 for (int j = 0; j < length; j++)
                 {
-
                     error += outputs[i, j].UpdateWeights(pattern, winner, iteration);
-
                 }
-
             }
-
             iteration++;
 
             return Math.Abs(error / (length * length));
-
         }
-
-
 
         private void DumpCoordinates()
         {
@@ -251,57 +187,65 @@ namespace Tugas
             for (int j = 0; j < patterns.Count; j++)
             {
                 Neuron n = Winner(patterns[j]);
-
                 Console.WriteLine("{0},{1},{2}", labels[j], n.X, n.Y);
             }
 
         }
 
+        private void CheckNeuronMap()
+        {
+            for (int i = 0; i < patterns.Count; i++)
+            {
+                Neuron n = Winner(patterns[i]);
+                Console.WriteLine("{0},{1},{2}", labels[i], n.X, n.Y);
+            }
+            int err = 0;
+            for (int i = 0; i < patterns.Count; i++)
+            {
+                int min = i;
+                Neuron n1 = Winner(patterns[min]);
+                for (int j = i + 1; j < patterns.Count; j++)
+                {
 
+                    Neuron n2 = Winner(patterns[j]);
+
+                    if (n1.X == n2.X && n1.Y == n2.Y && labels[j] != labels[min])
+                    {
+                        err++;
+                    }
+                }
+            }
+            Console.WriteLine("error: {0}", err);
+        }
 
         private Neuron Winner(double[] pattern)
         {
 
             Neuron winner = null;
-
             double min = double.MaxValue;
 
             for (int i = 0; i < length; i++)
-
                 for (int j = 0; j < length; j++)
                 {
-
                     double d = Distance(pattern, outputs[i, j].Weights);
-
                     if (d < min)
                     {
-
                         min = d;
-
                         winner = outputs[i, j];
-
                     }
-
                 }
-
             return winner;
-
         }
 
 
-
+        /*euclidean distance between neuron input and output*/
         private double Distance(double[] vector1, double[] vector2)
         {
-
             double value = 0;
-
             for (int i = 0; i < vector1.Length; i++)
             {
-
                 value += Math.Pow((vector1[i] - vector2[i]), 2);
-
             }
-
             return Math.Sqrt(value);
 
         }
--- Tugas/Tugas/Neuron.cs
+++ Tugas/Tugas/Neuron.cs
@@ -8,79 +8,52 @@ namespace Tugas
     class Neuron
     {
         public double[] Weights;
-
         public int X;
-
         public int Y;
-
         private int length;
-
         private double nf;
-
-
-
+        
+        /* setting the neuron */
         public Neuron(int x, int y, int length)
         {
-
             X = x;
-
             Y = y;
-
             this.length = length;
-
             nf = 1000 / Math.Log(length);
-
         }
 
-
-
+        /*gaussian function*/
         private double Gauss(Neuron win, int it)
         {
-
+            //euclidean distance
             double distance = Math.Sqrt(Math.Pow(win.X - X, 2) + Math.Pow(win.Y - Y, 2));
-
             return Math.Exp(-Math.Pow(distance, 2) / (Math.Pow(Strength(it), 2)));
-
         }
 
-
-
+        /*update learning rate*/
         private double LearningRate(int it)
         {
-
             return Math.Exp(-it / 1000) * 0.1;
-
         }
 
-
-
+        /*the variance parameter specifying the spread of the Gaussian function.*/
         private double Strength(int it)
         {
-
             return Math.Exp(-it / nf) * length;
-
         }
 
-
-
+        /*Updating weight equation*/
         public double UpdateWeights(double[] pattern, Neuron winner, int it)
         {
-
             double sum = 0;
-
             for (int i = 0; i < Weights.Length; i++)
             {
 
                 double delta = LearningRate(it) * Gauss(winner, it) * (pattern[i] - Weights[i]);
-
                 Weights[i] += delta;
-
                 sum += delta;
-
             }
-
             return sum / Weights.Length;
-
         }
     }
 }
